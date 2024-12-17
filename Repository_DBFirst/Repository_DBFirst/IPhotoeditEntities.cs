47e97c743a50ca3725d1426a5cc717b8255026f3
VadimOr02 <vadim.orghian@student.usv.ro>
2024-12-17 18:58:12 +0200

    Add files via upload

---
+++ Repository_DBFirst/Repository_DBFirst/IPhotoeditEntities.cs
@@ -0,0 +1,16 @@
+﻿using Repository_DBFirst;
+using System.Linq;
+using System.Data.Entity;
+using System;
+
+namespace Repository_DBFirst
+{
+    public interface IPhotoeditEntities : IDisposable
+    {
+        DbSet<Fotografii> Fotografii { get; set; }
+        DbSet<Abonamente> Abonamente { get; set; }
+        DbSet<Edits> Edits { get; set; }
+        DbSet<Utilizatori> Utilizatori { get; set; }
+        int SaveChanges();  // Trebuie să existe o metodă SaveChanges pentru a salva modificările în baza de date
+    }
+}
---
+++ Repository_DBFirst/Repository_DBFirst/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Repository_DBFirst")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Repository_DBFirst")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("903b7b91-c105-436b-b6f7-6e5617a4c460")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
